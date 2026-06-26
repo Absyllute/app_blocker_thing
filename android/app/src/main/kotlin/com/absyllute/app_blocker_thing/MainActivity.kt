3dd901ada960eaddd0ea514b27a1868f2b2dd97b
Absyllute <absyllute@gmail.com>
2026-06-26 06:15:45 +0200

    inital commit

---
+++ android/app/src/main/kotlin/com/absyllute/app_blocker_thing/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.absyllute.app_blocker_thing
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:app_blocker_thing/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const new({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      themeMode: .system,
+      theme: ThemeData(
+        colorScheme: .fromSeed(
+          seedColor: Colors.deepPurple,
+          brightness: .light,
+        ),
+      ),
+
+      darkTheme: ThemeData(
+        colorScheme: .fromSeed(
+          seedColor: Colors.deepPurple,
+          brightness: .light,
+        ),
+      ),
+
+      home: HomeScreen(),
+    );
+  }
+}
