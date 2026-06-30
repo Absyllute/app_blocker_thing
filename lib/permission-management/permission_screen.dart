0582ecdcfb70c4641bf727e478d8a71279703859
Absyllute <absyllute@gmail.com>
2026-06-30 08:44:08 +0200

    finalized permission request in permission_screen.dart

--- lib/permission-management/permission_screen.dart
+++ lib/permission-management/permission_screen.dart
@@ -1,3 +1,5 @@
+// if you think my code looks weird, its because of the built in flutter code formatting that i tried fighting with
+
 import 'package:app_blocker_thing/home/home_screen.dart';
 import 'package:flutter/material.dart';
 
@@ -8,7 +10,58 @@ class PermissionScreen extends StatefulWidget {
   State<PermissionScreen> createState() => PermissionScreenState();
 }
 
-class PermissionScreenState extends State<PermissionScreen> {
+class PermissionScreenState extends State<PermissionScreen>
+    with WidgetsBindingObserver {
+  Future<bool> hasOverlayPermissions() async {
+    final status = await appBlock.checkOverlayPermission();
+
+    if (status == 'granted') {
+      return true;
+    }
+
+    return false;
+  }
+
+  Future<bool> hasUsageStatsPermission() async {
+    final status = await appBlock.checkUsageStatsPermission();
+
+    if (status == 'granted') {
+      return true;
+    }
+
+    return false;
+  }
+
+  Future<bool> allPermissionsGranted() async {
+    final overlayStatus = await appBlock.checkOverlayPermission();
+    final usageStatsStatus = await appBlock.checkUsageStatsPermission();
+
+    if (overlayStatus == 'granted' && usageStatsStatus == 'granted') {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    if (state == AppLifecycleState.resumed) {
+      setState(() {});
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -39,13 +92,34 @@ class PermissionScreenState extends State<PermissionScreen> {
 
                         Spacer(),
 
-                        Text(
-                          'Denied',
-                          style: TextStyle(
-                            color: Colors.red,
-                            fontSize: 18,
-                            fontWeight: .bold,
-                          ),
+                        FutureBuilder(
+                          future: hasOverlayPermissions(),
+                          builder: (context, snapshot) {
+                            if (snapshot.connectionState ==
+                                ConnectionState.waiting) {
+                              return CircularProgressIndicator();
+                            }
+
+                            if (snapshot.hasData && snapshot.data == true) {
+                              return Text(
+                                'Granted',
+                                style: TextStyle(
+                                  color: Colors.green,
+                                  fontSize: 18,
+                                  fontWeight: .bold,
+                                ),
+                              );
+                            }
+
+                            return Text(
+                              'Denied',
+                              style: TextStyle(
+                                color: Colors.red,
+                                fontSize: 18,
+                                fontWeight: .bold,
+                              ),
+                            );
+                          },
                         ),
                       ],
                     ),
@@ -59,11 +133,26 @@ class PermissionScreenState extends State<PermissionScreen> {
                       children: [
                         Spacer(),
 
-                        ElevatedButton(
-                          onPressed: () async {
-                            await appBlock.requestOverlayPermission();
+                        FutureBuilder(
+                          future:
+                              hasOverlayPermissions(), // I was fighting with my code formatter and lost
+                          builder: (context, snapshot) {
+                            if (snapshot.connectionState ==
+                                ConnectionState.waiting) {
+                              return SizedBox();
+                            }
+
+                            if (snapshot.hasData && snapshot.data == true) {
+                              return SizedBox();
+                            }
+
+                            return ElevatedButton(
+                              onPressed: () async {
+                                await appBlock.requestOverlayPermission();
+                              },
+                              child: Text('Go To System Settings'),
+                            );
                           },
-                          child: Text('Go To System Settings'),
                         ),
                       ],
                     ),
@@ -91,13 +180,34 @@ class PermissionScreenState extends State<PermissionScreen> {
 
                         Spacer(),
 
-                        Text(
-                          'Granted',
-                          style: TextStyle(
-                            color: Colors.green,
-                            fontSize: 18,
-                            fontWeight: .bold,
-                          ),
+                        FutureBuilder(
+                          future: hasUsageStatsPermission(),
+                          builder: (context, snapshot) {
+                            if (snapshot.connectionState ==
+                                ConnectionState.waiting) {
+                              return CircularProgressIndicator();
+                            }
+
+                            if (snapshot.hasData && snapshot.data == true) {
+                              return Text(
+                                'Granted',
+                                style: TextStyle(
+                                  color: Colors.green,
+                                  fontSize: 18,
+                                  fontWeight: .bold,
+                                ),
+                              );
+                            }
+
+                            return Text(
+                              'Denied',
+                              style: TextStyle(
+                                color: Colors.red,
+                                fontSize: 18,
+                                fontWeight: .bold,
+                              ),
+                            );
+                          },
                         ),
                       ],
                     ),
@@ -112,16 +222,53 @@ class PermissionScreenState extends State<PermissionScreen> {
                       children: [
                         Spacer(),
 
-                        ElevatedButton(
-                          onPressed: () async {
-                            await appBlock.requestOverlayPermission();
+                        FutureBuilder(
+                          future: hasUsageStatsPermission(),
+                          builder: (context, snapshot) {
+                            if (snapshot.connectionState ==
+                                ConnectionState.waiting) {
+                              return SizedBox();
+                            }
+
+                            if (snapshot.hasData && snapshot.data == true) {
+                              return SizedBox();
+                            }
+
+                            return ElevatedButton(
+                              onPressed: () async {
+                                await appBlock.requestUsageStatsPermission();
+                              },
+                              child: Text('Go To System Settings'),
+                            );
                           },
-                          child: Text('Go To System Settings'),
                         ),
                       ],
                     ),
                   ],
                 ),
+              ),
+
+              Spacer(),
+
+              FutureBuilder(
+                future: allPermissionsGranted(),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return SizedBox();
+                  }
+
+                  if (snapshot.hasData && snapshot.data == true) {
+                    return SizedBox(
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        onPressed: () => setState(() {}),
+                        child: Text('Finish'),
+                      ),
+                    );
+                  }
+
+                  return SizedBox();
+                },
               ),
             ],
           ),
