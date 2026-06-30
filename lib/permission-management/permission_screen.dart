05ebfeff2f0a22354c8cef9a1e25359e6e6fda5c
Absyllute <absyllute@gmail.com>
2026-06-30 07:41:03 +0200

    added a basic permission screen (no funcitonallity)

--- lib/permission-management/permission_screen.dart
+++ lib/permission-management/permission_screen.dart
@@ -1,3 +1,4 @@
+import 'package:app_blocker_thing/home/home_screen.dart';
 import 'package:flutter/material.dart';
 
 class PermissionScreen extends StatefulWidget {
@@ -12,7 +13,120 @@ class PermissionScreenState extends State<PermissionScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Required Permissions')),
-      body: Center(child: Text('Permission Screen Content')),
+      body: Center(
+        child: Padding(
+          padding: .all(18),
+          child: Column(
+            spacing: 24,
+            children: [
+              //\\//\\//\\//\\//\\ -- Overlay Permission Container -- //\\//\\//\\//\\//\\
+              Container(
+                padding: .all(18),
+                decoration: BoxDecoration(
+                  color: Colors.grey[850],
+                  borderRadius: .circular(12),
+                ),
+
+                child: Column(
+                  spacing: 12,
+                  children: [
+                    Row(
+                      children: [
+                        Text(
+                          'Overlay Permission:',
+                          style: TextStyle(fontSize: 18, fontWeight: .bold),
+                        ),
+
+                        Spacer(),
+
+                        Text(
+                          'Denied',
+                          style: TextStyle(
+                            color: Colors.red,
+                            fontSize: 18,
+                            fontWeight: .bold,
+                          ),
+                        ),
+                      ],
+                    ),
+
+                    Text(
+                      textAlign: .center,
+                      'The "appear over other apps" allows the app to display a blocking overlay when the blocked app is opened.',
+                    ),
+
+                    Row(
+                      children: [
+                        Spacer(),
+
+                        ElevatedButton(
+                          onPressed: () async {
+                            await appBlock.requestOverlayPermission();
+                          },
+                          child: Text('Go To System Settings'),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+
+              //\\//\\//\\//\\//\\ -- Usage Stats Permission Container -- //\\//\\//\\//\\//\\
+              Container(
+                padding: .all(18),
+                decoration: BoxDecoration(
+                  color: Colors.grey[850],
+                  borderRadius: .circular(12),
+                ),
+
+                child: Column(
+                  spacing: 12,
+                  children: [
+                    Row(
+                      children: [
+                        Text(
+                          'Usage Stats Permission:',
+                          style: TextStyle(fontSize: 18, fontWeight: .bold),
+                        ),
+
+                        Spacer(),
+
+                        Text(
+                          'Granted',
+                          style: TextStyle(
+                            color: Colors.green,
+                            fontSize: 18,
+                            fontWeight: .bold,
+                          ),
+                        ),
+                      ],
+                    ),
+
+                    Text(
+                      textAlign: .center,
+                      'The "Usage Stats" permission allows the app to see what app is currently open.'
+                      'In order to detect if the blocked app is opened, we need the usage stats permission',
+                    ),
+
+                    Row(
+                      children: [
+                        Spacer(),
+
+                        ElevatedButton(
+                          onPressed: () async {
+                            await appBlock.requestOverlayPermission();
+                          },
+                          child: Text('Go To System Settings'),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
